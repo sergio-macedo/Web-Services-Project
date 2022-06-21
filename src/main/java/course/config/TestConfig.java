bb6c874d423f63a36a96f894cfeda497714579a5
sergio-macedo <sergio.garoma@hotmail.com>
2022-06-21 19:27:00 +0200

    categories are done

--- src/main/java/course/config/TestConfig.java
+++ src/main/java/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package course.config;
 
+import course.entities.Category;
 import course.entities.Order;
 import course.entities.User;
 import course.entities.enums.OrderStatus;
+import course.repositories.CategoryRepository;
 import course.repositories.OrderRepository;
 import course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,17 +20,30 @@ import java.util.Arrays;
 public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
+
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category c1 = new Category(null,"Eletronics");
+        Category c2 = new Category(null,"Books");
+        Category c3 = new Category(null,"Computers");
+
+        categoryRepository.saveAll(Arrays.asList(c1,c2,c3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.DELIVERED,u1);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.PAID,u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.CANCELED, u1);
+
+
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
