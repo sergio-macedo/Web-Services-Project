26d8a22ea2235d9bf52c3159cfa591a5ba344d5a
sergio-macedo <sergio.garoma@hotmail.com>
2022-06-22 11:47:56 +0200

    product class

---
+++ src/main/java/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package course.repositories;
+
+import course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
---
+++ src/main/java/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package course.services;
+
+import course.entities.Product;
+import course.entities.User;
+import course.repositories.ProductRepository;
+import course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
