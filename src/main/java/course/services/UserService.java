70e3acdb66c89f45b1732975287ee82b0bae6bab
sergio-macedo <sergio.garoma@hotmail.com>
2022-06-23 14:04:38 +0200

    Exception handling delete

--- src/main/java/course/services/UserService.java
+++ src/main/java/course/services/UserService.java
@@ -2,8 +2,11 @@ package course.services;
 
 import course.entities.User;
 import course.repositories.UserRepository;
+import course.services.exceptions.DatabaseException;
 import course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,14 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {  //there are easier ways to do an Update at the Crud.
